acf6ea3e15e4c00bd1159bae65cf95f8fbd641ee
Dylan Murray <dymurray@redhat.com>
2019-11-20 23:05:34 -0500

    server start

--- v2/react-backend/hack/start-servers.sh
+++ v2/react-backend/hack/start-servers.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 source ~/.tr-config/main1
 PORT=3001 pm2 start -f /root/treerat/v2/react-backend/bin/www -- 3001
+source ~/.tr-config/main2
+PORT=3002 pm2 start -f /root/treerat/v2/react-backend/bin/www -- 3002
+source ~/.tr-config/main3
+PORT=3003 pm2 start -f /root/treerat/v2/react-backend/bin/www -- 3003
+source ~/.tr-config/main4
+PORT=3004 pm2 start -f /root/treerat/v2/react-backend/bin/www -- 3004
+source ~/.tr-config/main5
+PORT=3005 pm2 start -f /root/treerat/v2/react-backend/bin/www -- 3005
