2b886e57a1fff76fa23ae9d08a961da5194c29bd
Dylan Murray <dymurray@redhat.com>
2019-11-20 22:09:35 -0500

    clear

--- v2/react-backend/lib/jigs.js
+++ v2/react-backend/lib/jigs.js
@@ -76,29 +76,29 @@ class BadReviewToken extends Run.Token {
 }
 BadReviewToken.originTestnet = 'e325e07d25399be674a3f3a48479e63c36d3873f7dfb085e199a900c78dcd9dd_o1'
 BadReviewToken.locationTestnet = 'e325e07d25399be674a3f3a48479e63c36d3873f7dfb085e199a900c78dcd9dd_o1'
-BadReviewToken.originMainnet = '6461d89947b0eaf8b1b84af88dc4cfe33349c9605c595921a38f9ed91e6ed225_o1'
-BadReviewToken.locationMainnet = '6461d89947b0eaf8b1b84af88dc4cfe33349c9605c595921a38f9ed91e6ed225_o1'
+BadReviewToken.originMainnet = ''
+BadReviewToken.locationMainnet = ''
 
 class TrueReviewAlphaTesterToken extends Run.Token {
 }
 TrueReviewAlphaTesterToken.originTestnet = '674d3335e8648b1bcc3f072328d542a5033ce090d1ef84f977b915ff1ef7a290_o1'
 TrueReviewAlphaTesterToken.locationTestnet = '674d3335e8648b1bcc3f072328d542a5033ce090d1ef84f977b915ff1ef7a290_o1'
-TrueReviewAlphaTesterToken.originMainnet = 'f1f8b1c8cd1074600a408465c81c89f5d4caaeba57364a5f2440896bd4a41042_o1'
-TrueReviewAlphaTesterToken.locationMainnet = 'f1f8b1c8cd1074600a408465c81c89f5d4caaeba57364a5f2440896bd4a41042_o1'
+TrueReviewAlphaTesterToken.originMainnet = ''
+TrueReviewAlphaTesterToken.locationMainnet = ''
 
 class ReviewToken extends Run.Token {
 }
 ReviewToken.originTestnet = 'f41a25a9342db5fb5d4a09f45d56aa11a038c78c2f6396e396d8be03152dd50a_o1'
 ReviewToken.locationTestnet = 'f41a25a9342db5fb5d4a09f45d56aa11a038c78c2f6396e396d8be03152dd50a_o1'
-ReviewToken.originMainnet = 'b167a40401eabef31eabffec76a664faf23a1f5244ee27dad8f443cca7e777e1_o1'
-ReviewToken.locationMainnet = 'b167a40401eabef31eabffec76a664faf23a1f5244ee27dad8f443cca7e777e1_o1'
+ReviewToken.originMainnet = ''
+ReviewToken.locationMainnet = ''
 
 class VoteToken extends Run.Token {
 }
 VoteToken.originTestnet = '431e9dd48509aef8a9f5f0a52e77f57944340f0a1947306444c05a77daa0eda4_o1'
 VoteToken.locationTestnet = '431e9dd48509aef8a9f5f0a52e77f57944340f0a1947306444c05a77daa0eda4_o1'
-VoteToken.originMainnet = 'd124d33d391b23a4dc5e588792ad3a17d4c0776475616e846d958adfd7ff758f_o1'
-VoteToken.locationMainnet = 'd124d33d391b23a4dc5e588792ad3a17d4c0776475616e846d958adfd7ff758f_o1'
+VoteToken.originMainnet = ''
+VoteToken.locationMainnet = ''
 
 
 class AllLocations extends Run.Jig {
@@ -107,8 +107,8 @@ class AllLocations extends Run.Jig {
 }
 AllLocations.originTestnet = '62da8bb61226cc9177ffa9eabadbf0fc374705715909f3af796a037bd37bd95e_o1'
 AllLocations.locationTestnet = '62da8bb61226cc9177ffa9eabadbf0fc374705715909f3af796a037bd37bd95e_o1'
-AllLocations.originMainnet = 'a024060a425e50eb76a3b9647f04dc59dbd9bdc5b36a4abe57e02b179fac8129_o1'
-AllLocations.locationMainnet = 'a024060a425e50eb76a3b9647f04dc59dbd9bdc5b36a4abe57e02b179fac8129_o1'
+AllLocations.originMainnet = ''
+AllLocations.locationMainnet = ''
 
 class Location extends Run.Jig {
   init(placeID, coords, name) {
@@ -136,8 +136,8 @@ class Location extends Run.Jig {
 }
 Location.originTestnet = 'b2343c9cec98c547604edddd0675a27fd9420d523e988ef114ba50eac51751b6_o1'
 Location.locationTestnet = 'b2343c9cec98c547604edddd0675a27fd9420d523e988ef114ba50eac51751b6_o1'
-Location.originMainnet = '4bd9ab6f365627cad1fdbbeb50459b3d4baa69da8372a7f8dc5e4bec0bf0c502_o1'
-Location.locationMainnet = '4bd9ab6f365627cad1fdbbeb50459b3d4baa69da8372a7f8dc5e4bec0bf0c502_o1'
+Location.originMainnet = ''
+Location.locationMainnet = ''
 
 class ReviewPointsDB extends Run.Jig {
   set(key, value) {this[key] = value}
@@ -159,8 +159,8 @@ class ReviewPointsDB extends Run.Jig {
 }
 ReviewPointsDB.originTestnet = 'dee4bfc82701c991a451d32c02b7b8ebb7ccd6f50a49e3b217e735170e670135_o1'
 ReviewPointsDB.locationTestnet = 'dee4bfc82701c991a451d32c02b7b8ebb7ccd6f50a49e3b217e735170e670135_o1'
-ReviewPointsDB.originMainnet = '3c762ff4579a72ab6aa7b07015ace49bdeadb1772bc33c347c2bc034c30b5d9f_o1'
-ReviewPointsDB.locationMainnet = '3c762ff4579a72ab6aa7b07015ace49bdeadb1772bc33c347c2bc034c30b5d9f_o1'
+ReviewPointsDB.originMainnet = ''
+ReviewPointsDB.locationMainnet = ''
 
 class Review extends Run.Jig {
   init(reviewLocation, body, rating, user) {
@@ -175,8 +175,8 @@ class Review extends Run.Jig {
 }
 Review.originTestnet = 'b81a51b2482bfd3e3f985dc45eca220a806a7dabea36c493431253218cf00a2d_o1'
 Review.locationTestnet = 'b81a51b2482bfd3e3f985dc45eca220a806a7dabea36c493431253218cf00a2d_o1'
-Review.originMainnet = 'e978dedd616d550735d9b1f9349eff91b0993da6daac0dab0eae98a43a1266eb_o1'
-Review.locationMainnet = 'e978dedd616d550735d9b1f9349eff91b0993da6daac0dab0eae98a43a1266eb_o1'
+Review.originMainnet = ''
+Review.locationMainnet = ''
 
 Location.deps = {TrueReview, Review, BusinessCoupon}
 
